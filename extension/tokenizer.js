802f61d17d6ae15356be189421015dd937393e10
Okhra <66963445+okhra-dev@users.noreply.github.com>
2026-03-15 20:58:30 +0300

    Update tokenizer.js

--- extension/tokenizer.js
+++ extension/tokenizer.js
@@ -1,65 +1,87 @@
-const ASCII_PUNCT = "!\"#$%&()*+,-./:;<=>?@[\\]^_`{|}~";
-const EXT_PUNCT   = "\u2013\u2014\u2026\u00AB\u00BB\u2039\u203A" +
-                    "\u201C\u201D\u2018\u2019\u201E\u201A" +
-                    "\u2010\u2011\u2012\u2015\u201F\u201B";
-const PUNCT_SET   = new Set([...ASCII_PUNCT, ...EXT_PUNCT]);
+var PUNCT_CHARS = "!\"#$%&()*+,-./:;<=>?@[\\]^_`{|}~" +
+  "\u2013\u2014\u2026\u00AB\u00BB\u2039\u203A" +
+  "\u201C\u201D\u2018\u2019\u201E\u201A" +
+  "\u2010\u2011\u2012\u2015\u201F\u201B";
+var PUNCT_SET = {};
+for (var i = 0; i < PUNCT_CHARS.length; i++) {
+  PUNCT_SET[PUNCT_CHARS[i]] = true;
+}
 
 function separatePunct(text) {
-  let out = "";
-  for (const ch of text) {
-    out += PUNCT_SET.has(ch) ? ("  " + ch + "  ") : ch;
+  var out = "";
+  for (var i = 0; i < text.length; i++) {
+    var ch = text[i];
+    if (PUNCT_SET[ch]) {
+      out += "  " + ch + "  ";
+    } else {
+      out += ch;
+    }
   }
   return out;
 }
 
 function tokenizeText(text, selectWords) {
-  const lcWords  = separatePunct(text.toLowerCase()).split(/\s+/).filter(Boolean);
-  const dispWords = separatePunct(text).split(/\s+/).filter(Boolean);
-
-  const tokens = lcWords.map(t =>
-    (PUNCT_SET.has(t) || selectWords.has(t)) ? t : "_"
-  );
-  return { tokens, displayWords: dispWords };
+  var raw = separatePunct(text);
+  var rawLc = separatePunct(text.toLowerCase());
+  var dispTokens = raw.split(/\s+/).filter(function(t) { return t.length > 0; });
+  var lcTokens = rawLc.split(/\s+/).filter(function(t) { return t.length > 0; });
+  var synTokens = [];
+  for (var i = 0; i < lcTokens.length; i++) {
+    var t = lcTokens[i];
+    if (PUNCT_SET[t] || selectWords.has(t)) {
+      synTokens.push(t);
+    } else {
+      synTokens.push("_");
+    }
+  }
+  return { tokens: synTokens, displayWords: dispTokens };
 }
 
 function tokensToIndices(tokens, vocabMap, unkIdx) {
-  return tokens.map(t => (vocabMap[t] !== undefined ? vocabMap[t] : unkIdx));
+  var out = [];
+  for (var i = 0; i < tokens.length; i++) {
+    var idx = vocabMap[tokens[i]];
+    out.push(idx !== undefined ? idx : unkIdx);
+  }
+  return out;
 }
 
 function chunkIndices(indices, maxLen, overlap) {
-  const step   = maxLen - overlap;
-  const chunks = [];
-  const starts = [];
-
+  var step = maxLen - overlap;
+  var chunks = [];
+  var starts = [];
   if (indices.length <= maxLen) {
     chunks.push(indices.slice());
     starts.push(0);
   } else {
-    for (let i = 0; i <= indices.length - maxLen; i += step) {
+    for (var i = 0; i <= indices.length - maxLen; i += step) {
       chunks.push(indices.slice(i, i + maxLen));
       starts.push(i);
     }
-    const tailStart = indices.length - maxLen;
+    var tailStart = indices.length - maxLen;
     if (starts[starts.length - 1] !== tailStart) {
       chunks.push(indices.slice(tailStart));
       starts.push(tailStart);
     }
   }
-  return { chunks, starts };
+  return { chunks: chunks, starts: starts };
 }
 
-function mapProbsToWords(probs, starts, chunks, numWords) {
-  const sum   = new Float32Array(numWords);
-  const count = new Float32Array(numWords);
-
-  for (let ci = 0; ci < chunks.length; ci++) {
-    for (let j = 0; j < chunks[ci].length; j++) {
-      const wi = starts[ci] + j;
+function mapChunkProbsToWords(chunkProbs, starts, chunks, numWords) {
+  var sum = new Float32Array(numWords);
+  var count = new Float32Array(numWords);
+  for (var ci = 0; ci < chunks.length; ci++) {
+    for (var j = 0; j < chunks[ci].length; j++) {
+      var wi = starts[ci] + j;
       if (wi < numWords) {
-        sum[wi]   += probs[ci];
+        sum[wi] += chunkProbs[ci];
         count[wi] += 1;
       }
     }
   }
-  return Array.from(sum, (s, i) => (count[i] > 0 ? s / count[i] : 0));
+  var out = [];
+  for (var i = 0; i < numWords; i++) {
+    out.push(count[i] > 0 ? sum[i] / count[i] : 0);
+  }
+  return out;
 }
